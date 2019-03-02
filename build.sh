102a0e7bf61ab3fa63366b3fc81b0013d2e9f58e
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2019-03-02 23:21:00 +0700

    Update and rename build to build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+rm -f /tmp/${CUDA_REPO_PKG}
+sudo apt-get update
+sudo apt-get install cuda-drivers -y
+sudo apt-get install cuda -y
