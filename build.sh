90caf6f1f8d738b3bd8982a3a1d2da102222eaac
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2019-03-02 23:40:00 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+sudo apt-get install gcc-5 g++-5 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1
 CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
 sudo dpkg -i /tmp/${CUDA_REPO_PKG}
@@ -10,3 +12,6 @@ rm -f /tmp/${CUDA_REPO_PKG}
 sudo apt-get update
 sudo apt-get install cuda-drivers -y
 sudo apt-get install cuda -y
+https://github.com/tpruvot/ccminer
+cd ccminer
+./build.sh
