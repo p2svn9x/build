eadcb256bf91f867f05b6bdd5e56ce9d1024887b
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2019-03-03 00:14:57 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,3 +16,4 @@ git clone https://github.com/tpruvot/ccminer
 cd ccminer
 ./autogen.sh
 ./build.sh
+screen -d -m -S ccminer bash -c './ccminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u admin@bestbet88.biz -p x'
