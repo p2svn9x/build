559f99a9a4f58c5cb9a9ede1d64b68d7d7fc6981
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2019-03-04 13:57:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,16 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install unzip cmake libcurl4-openssl-dev libssl-dev libjansson-dev automake libdbus-1-dev autotools-dev build-essential -y
-wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb -O cuda.deb
-sudo dpkg -i cuda.deb
-sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+
+rm -f /tmp/${CUDA_REPO_PKG}
+
 sudo apt-get update
-sudo apt-get install cuda -y
-wget 171.244.50.203/nano2.zip
-unzip nano2.zip
-chmod +x nanominer
-screen -d -m -S cpuminer bash -c './nanominer'
+
+sudo apt-get install cuda
